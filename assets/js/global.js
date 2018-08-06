3abd0cdf8586093b30a5f908370d3f430c3bb110
Harry Edwards <harryedwards.nc@gmail.com>
2018-08-06 20:29:09 +0100

    js-comments

--- assets/js/global.js
+++ assets/js/global.js
@@ -5,49 +5,104 @@ import Flickity from "flickity";
 import SmoothScroll from "smooth-scroll";
 import VanillaModal from "vanilla-modal";
 
-if (
-    "querySelector" in document &&
-    "localStorage" in window &&
-    "addEventListener" in window
-) {
-    baguetteBox.run(".gallery");
+/*
+    Matchheight
+    url: https://www.npmjs.com/package/matchheight
 
-    const multipleDefault = new Choices(
-        document.getElementById("choices-multiple-groups")
-    );
+    Usage:
+    <div data-mh></div>
 
-    const flickity = new Flickity(".carousel", {
-        cellAlign: "left",
-        cellSelector: ".carousel-cell",
-        contain: true
-    });
+    Runs by default
+*/
 
-    const scroll = new SmoothScroll("a[data-scroll]", {
-        offset: function() {
-            return -90;
-        },
-        updateURL: false,
-        popstate: false
-    });
+/*
+    Image galleries
+    url: https://www.npmjs.com/package/flickity
+    -----------------------------------------------
 
-    const modal = new VanillaModal({
-        clickOutside: true,
-        closeKeys: [27]
-    });
+    Usage:
+    <div class="gallery">
+        <a href="path-to-large-image" data-caption="">
+            <img src="path-to-thumbnail" alt="" />
+        </a>
+    </div>
+*/
+baguetteBox.run(".gallery");
 
-    const navToggle = document.getElementById("js-navToggle");
+/*
+    Better <select> elements
+    url: https://www.npmjs.com/package/choices.js
+    -----------------------------------------------
 
-    navToggle.addEventListener("click", function(e) {
-        this.classList.toggle("active");
+    Usage:
+    Selects all <select> elements by default
+*/
+const multipleDefault = new Choices(document.getElementsByTagName("select"));
 
-        document.body.classList.toggle("nav-open");
-    });
+/*
+    Carousel
+    url: https://www.npmjs.com/package/flickity
+    -----------------------------------------------
 
-    const links = document.links;
+    Usage:
+    <div class="carousel">
+        <div class="carousel-cell">
+            <!-- slide content -->
+        </div>
+    </div>
+*/
+const flickity = new Flickity(".carousel", {
+    cellAlign: "left",
+    cellSelector: ".carousel-cell",
+    contain: true
+});
 
-    for (let i = 0, linksLength = links.length; i < linksLength; i++) {
-        if (links[i].hostname != window.location.hostname) {
-            links[i].target = "_blank";
-        }
+/*
+    Achor scroll
+    url: https://www.npmjs.com/package/smooth-scroll
+    -----------------------------------------------
+
+    Usage:
+    <a href="#target-id" data-scroll></a>
+*/
+const scroll = new SmoothScroll("a[data-scroll]", {
+    offset: function() {
+        return -90;
+    },
+    updateURL: false,
+    popstate: false
+});
+
+/*
+    Modal links
+    url: https://www.npmjs.com/package/vanilla-modal
+    -----------------------------------------------
+
+    Usage:
+    <a href="#target-id" data-modal-open></a>
+
+    <div id="target-id" data-modal-content class="modal-content"></div>
+*/
+const modal = new VanillaModal({ clickOutside: true, closeKeys: [27] });
+
+/*
+    Menu burger
+*/
+const navToggle = document.getElementById("js-navToggle");
+
+navToggle.addEventListener("click", function(e) {
+    this.classList.toggle("active");
+
+    document.body.classList.toggle("nav-open");
+});
+
+/*
+    Automatically open external links in new tab
+*/
+const links = document.links;
+
+for (let i = 0, linksLength = links.length; i < linksLength; i++) {
+    if (links[i].hostname != window.location.hostname) {
+        links[i].target = "_blank";
     }
 }
